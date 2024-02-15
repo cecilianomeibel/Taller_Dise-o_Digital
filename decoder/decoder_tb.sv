63eee8d908f7986f1362efb799d5da1138916d10
Meibel Ceciliano <cecilianomeibel@gmail.com>
2024-02-15 15:16:16 -0600

    More efficient testbench code change and bug fixes

--- decoder/decoder_tb.sv
+++ decoder/decoder_tb.sv
@@ -4,7 +4,7 @@ module decoder_tb();
     logic [3:0] binary;
     
     //Outputs
-    logic [7:0] bcd;
+    logic [5:0] bcd;
     
     //Se instancia el modulo decoder
     decoder dut(
@@ -14,35 +14,85 @@ module decoder_tb();
     
     
     initial begin
-        // Prueba todos los posibles valores de entrada
-        for (int i = 0; i < 16; i++) begin
-            binary = i; // Ajustar la entrada binaria al valor actual de i
-            
-            #5; // Para esperar una cantidad de tiempo
-            
-            // Verifica la salidad con los valores de BCD definidos en decoder
-            case(i)
-                4'b0000: assert(bcd == 8'b0000_0000); //0 en BCD
-                4'b0001: assert(bcd == 8'b0000_0001); //1 en BCD
-                4'b0010: assert(bcd == 8'b0000_0010); //2 en BCD
-                4'b0011: assert(bcd == 8'b0000_0011); //3 en BCD
-                4'b0100: assert(bcd == 8'b0000_0100); //4 en BCD
-                4'b0101: assert(bcd == 8'b0000_0101); //5 en BCD
-                4'b0110: assert(bcd == 8'b0000_0110); //6 en BCD
-                4'b0111: assert(bcd == 8'b0000_0111); //7 en BCD
-                4'b1000: assert(bcd == 8'b0000_1000); //8 en BCD
-                4'b1001: assert(bcd == 8'b0000_1001); //9 en BCD
-                4'b1010: assert(bcd == 8'b0001_0000); //10 en BCD
-                4'b1011: assert(bcd == 8'b0001_0001); //11 en BCD
-                4'b1100: assert(bcd == 8'b0001_0010); //12 en BCD
-                4'b1101: assert(bcd == 8'b0001_0011); //13 en BCD
-                4'b1110: assert(bcd == 8'b0001_0100); //14 en BCD
-                4'b1111: assert(bcd == 8'b0001_0101); //15 en BCD
-                default: assert(0); //caso no esperado que no coincide con ninguna opcion
-            endcase
-        end
+        // Prueba posibles valores de entrada
+		  
+		  // Prueba 1: entrada 4'b0000
+        binary = 4'b0000;	
+		  #10; // Añade un retardo para permitir que bcd se actualice
+		  if (bcd == 6'b00_0000) $display("Prueba exitosa");
+		  if (bcd != 6'b00_0000) $display("Prueba fallida");
+		  #20 // Para esperar una cantidad de tiempo
+		 
+		  // Prueba 2: entrada 4'b0001
+        binary = 4'b0001;
+		  #10; // Añade un retardo para permitir que bcd se actualice
+		  if (bcd == 6'b00_0001) $display("Prueba exitosa");
+		  if (bcd != 6'b00_0001) $display("Prueba fallida");
+		  #20 // Para esperar una cantidad de tiempo
+		  
+		   // Prueba 3: entrada b0010
+        binary = 4'b0010;
+		  #10; // Añade un retardo para permitir que bcd se actualice
+		  if (bcd == 6'b00_0010) $display("Prueba exitosa");
+		  if (bcd != 6'b00_0010) $display("Prueba fallida");
+		  #20 // Para esperar una cantidad de tiempo
+		  
+		   
+			// Prueba 4: entrada  4'b0011
+        binary =  4'b0011;
+		  #10; // Añade un retardo para permitir que bcd se actualice
+		  if (bcd == 6'b00_0011) $display("Prueba exitosa");
+		  if (bcd != 6'b00_0011) $display("Prueba fallida");
+		  #20 // Para esperar una cantidad de tiempo
+		  
+		  
+		  	// Prueba 5: entrada  4'b1111
+        binary =  4'b1111;
+		  #10; // Añade un retardo para permitir que bcd se actualice
+		  if (bcd == 6'b01_0101) $display("Prueba exitosa"); 
+		  if (bcd != 6'b01_0101) $display("Prueba fallida");
+		  #20 // Para esperar una cantidad de tiempo
+		  
+		  
+		  	// Prueba 6: entrada  4'b1110
+        binary =  4'b1110;
+		  #10; // Añade un retardo para permitir que bcd se actualice
+		  if (bcd == 6'b01_0100) $display("Prueba exitosa");
+		  if (bcd != 6'b01_0100) $display("Prueba fallida");
+		 #20 // Para esperar una cantidad de tiempo
+		 
+		 
+		 	// Prueba 7: entrada  4'b1101
+        binary =  4'b1101;
+		  #10; // Añade un retardo para permitir que bcd se actualice
+		  if (bcd == 6'b01_0011) $display("Prueba exitosa");
+		  if (bcd != 6'b01_0011) $display("Prueba fallida");
+		  #20 // Para esperar una cantidad de tiempo
+		  
+		  
+		  // Prueba 8: entrada  4'b1101
+        binary =  4'b1100;
+		  #10; // Añade un retardo para permitir que bcd se actualice
+		  if (bcd == 6'b01_0010) $display("Prueba exitosa");
+		  if (bcd != 6'b01_0010) $display("Prueba fallida");
+	     #20 // Para esperar una cantidad de tiempo
+		  
+		 
         $display("El Testbench finalizo correctamente");
-        //$finish; // Termina simulacion
+       
     end
 
 endmodule
+
+
+
+//Casos no probados en testbench pero si en valores forzados
+
+//4'b0100
+//4'b0101
+//4'b0110
+//4'b0111
+//4'b1000
+//4'b1001
+//4'b1010
+//4'b1011
