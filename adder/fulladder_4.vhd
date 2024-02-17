56541c3c5e1765b3ee3ed1e6e0da401f8c039b36
Carlos Contreras Luna <carlos.andres@estudiantec.cr>
2024-02-17 16:18:04 -0600

    Fixed problems with the FPGA and improved the comments
    Fixed a bug with pin assignment and changed some comments.

--- adder/fulladder_4.vhd
+++ adder/fulladder_4.vhd
@@ -1,49 +1,56 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
 entity fulladder_4 is
     port (
-        a, b : in std_logic_vector(3 downto 0); -- Entradas de dos números de 4 bits
-        Cin  : in std_logic;                   -- Entrada del acarreo de entrada
-        sum4  : out std_logic_vector(6 downto 0); -- Salida de la suma de 4 bits
-        Cout : out std_logic_vector(6 downto 0)                   -- Salida del acarreo de salida
-    );
+        a: in std_logic_vector(3 downto 0); 			-- Entrada del primer número de 4 bits
+		  b: in std_logic_vector(3 downto 0); 			-- Entrada del segundo número de 4 bits
+        Cin  : in std_logic;                   		-- Acarreo de entrada
+        sum4  : out std_logic_vector(6 downto 0); 	-- Salida de la suma de 4 bits
+        Cout : out std_logic_vector(6 downto 0) );  -- Salida del acarreo de salida
 end fulladder_4;
 
-architecture Behavioral of fulladder_4 is
+architecture Structural of fulladder_4 is
+
+	-- Instancia para utilizar el sumador de un 1-bit
     component fulladder_1 is
+	 
         port (
-            a, b, Cin : in std_logic; -- Entradas de los bits a sumar y el acarreo de entrada
-            sum1, Cout : out std_logic   -- Salidas de la suma y el acarreo de un solo bit
-        );
+            a : in std_logic; 
+				b : in std_logic;
+				Cin : in std_logic;
+            sum1 : out std_logic;
+				Cout: out std_logic );
+        
     end component;
 	 
 	 component disp7seg is
-		port (sum4 : in  STD_LOGIC_VECTOR (3 downto 0);
-				seg : out  STD_LOGIC_VECTOR (6 downto 0)
-		);
+	 
+		port (b_num : in  STD_LOGIC_VECTOR (3 downto 0);
+				seg : out  STD_LOGIC_VECTOR (6 downto 0) );
 	 
 	 end component;
-
-    signal C1, C2, C3, C4 : std_logic; -- Señales para almacenar los acarreos intermedios
 	 
-	 signal result: std_logic_vector(3 downto 0); --Señal que guarda el resultado de la suma
+	 signal C_int : std_logic_vector(3 downto 0); 		-- Vector para almacenar los acarreos intermedios
 	 
-	 signal carry : std_logic_vector(3 downto 0); -- Señal para almacenar el carry de salida
+	 signal result: std_logic_vector(3 downto 0); 		--Señal que guarda el resultado de la suma
+	 
+	 signal f_carry : std_logic_vector(3 downto 0); 	-- Señal para almacenar el carry de salida
 	 
 	 
 begin
     -- Instancias de sumadores de un bit, conectados en cascada
-    FA0: fulladder_1 port map (a(0), b(0), Cin, result(0), C1); -- Suma el bit menos significativo
-    FA1: fulladder_1 port map (a(1), b(1), C1, result(1), C2); -- Suma el segundo bit con el acarreo anterior
-    FA2: fulladder_1 port map (a(2), b(2), C2, result(2), C3); -- Suma el tercer bit con el acarreo anterior
-    FA3: fulladder_1 port map (a(3), b(3), C3, result(3), C4); -- Suma el bit más significativo y obtiene el acarreo de salida
+    FA0: fulladder_1 port map (a(0), b(0), Cin, result(0), C_int(0)); -- Suma el bit menos significativo
+    FA1: fulladder_1 port map (a(1), b(1), C_int(0), result(1), C_int(1)); -- Suma el segundo bit con el acarreo anterior
+    FA2: fulladder_1 port map (a(2), b(2), C_int(1), result(2), C_int(2)); -- Suma el tercer bit con el acarreo anterior
+    FA3: fulladder_1 port map (a(3), b(3), C_int(2), result(3), C_int(3)); -- Suma el bit más significativo y obtiene el acarreo de salida
 	 
-	 carry <= "000" & C4;	--Guarda el carry de salida como un número de 4 bits
+	 f_carry <= "000" & C_int(3);	--Guarda el carry de salida como un número de 4 bits
 	 
+	 --Conversor de las salidas a números de 7 bits correspondientes
+	 --a cada segmento de los display utilizados
 	 
-	 hex1: disp7seg port map(result, sum4); 
-	 hex2: disp7seg port map(carry, Cout);
+	 hex1: disp7seg port map(result, sum4);  --Convierte el resultado de la suma en un número de 7 bits 
+	 hex2: disp7seg port map(f_carry, Cout);	  --Convierte el carry final en un número de 7 bits 
 	 
-end Behavioral;
+end Structural;
