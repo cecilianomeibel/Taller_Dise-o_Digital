aaae148d14969cb084f2bb6c1e42974121cb88e7
LudwinR <lujorabri@gmail.com>
2024-03-01 19:29:03 -0600

    Creation a testbench for resta module
    It's create a testbench for simulate resta module

--- Resta_And_Xor_ShiftLeft/resta.sv
+++ Resta_And_Xor_ShiftLeft/resta.sv
@@ -9,18 +9,18 @@ module resta #(parameter N = 4)(
 	logic cin;
 	
 	genvar j;
-	integer i;
-	
-	//restador #(N) restador_nums(.carry_num_in(carry_num),.A_num(A_num),.bin(bin),.carry_num_out(carry_num),.result(result))
+	//integer i;
 	
 	always @(*) begin
 		cin <= 1'b1;
 		carry_num [0] <= cin;
 		
-		for (i = 0; i < N; i = i+1) begin: mult_loop1
-			bin[i] <= B_num[i] ^ cin;
+		bin <= ~B_num;
 		
-		end
+//		for (i = 0; i < N; i = i+1) begin: mult_loop1
+//			bin[i] <= B_num[i] ^ cin;
+//		
+//		end
 	end
 	
 	
@@ -35,29 +35,6 @@ module resta #(parameter N = 4)(
 
 	
 endmodule
-
-/*
-module restador #(parameter N = 4)(
-	//input logic [N:0] carry_num_in,
-	input logic [N-1:0] A_num,
-	input logic [N-1:0] bin,
-	output logic [N:0] carry_num_out, 
-	output logic [N-1:0] result);
-	
-	genvar j;
-	
-	logic [N:0] carry_num;
-	
-	generate
-		for (j = 0; j < N; j = j+1) begin: mult_loop2
-		
-			restar resta_nums(.carry_num(carry_num_out[j+1]),.sum(result[j]),.ain(A_num[j]),.bin(bin[j]),.cin(carry_num_in[j]));
-		end
-		
-	endgenerate
-	
-
-endmodule */
 
 
 module restar(
