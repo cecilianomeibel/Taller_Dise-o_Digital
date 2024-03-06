c3c6635b553e1e29fc6467d1f9fe118940e486cb
LudwinR <lujorabri@gmail.com>
2024-03-06 11:44:53 -0600

    Correction problem2

--- Problema 2/Main.sv
+++ Problema 2/Main.sv
@@ -1,21 +1,24 @@
-module Main(
-     
+module Main
+     #(parameter N = 4)
+	 (
+	  //Se definen las entradas 	  
+	  input logic [N-1:0] A_num, B_num,operations_buttons,change_mode,
+	  input logic clk, reset,
+	  
+	  //Se definen las salidas 
+	 
       output logic [6:0] a,
-		output logic [6:0] b,
-		output logic [6:0] c,
-		output logic [6:0] d
+	  output logic [6:0] b,
+	  output logic [6:0] c,
+      output logic [6:0] d
 );
 
 
-parameter N = 2;
 
 
 
-//Definir las variables 
-reg [N-1:0] A_num;
-reg [N-1:0] B_num;
-reg [3:0] operations_buttons;
-reg [1:0] change_mode;
+//Definir las variables intermedias 
+
 reg [6:0] seg1;
 reg [6:0] seg2;
 reg [6:0] result_seg;
@@ -33,17 +36,9 @@ reg [6:0] Oseg2;
 reg [6:0] Oresult_seg;   
 reg [6:0] Oflags_seg;
 
-//reg [6:0] a;
-//reg [6:0] b;
-//reg [6:0] c;
-//reg [6:0] d;
+  
 
-
-logic clk;
-logic reset;  
-	  
-
-//Instanciar los 3 modulos
+//Se instancian los 3 modulos ALU, FlipFlop1,FlipFlop2
 
 FlipFlop1 ff1_inst (.clk(clk),.reset(reset),.A_numf1(A_num),.B_numf1(B_num),.operations_buttonsf1(operations_buttons),
 .change_modef1(change_mode),.Output_A_numf1(Output_A_num1),.Output_B_numf1(Output_B_num1),
@@ -52,26 +47,12 @@ FlipFlop1 ff1_inst (.clk(clk),.reset(reset),.A_numf1(A_num),.B_numf1(B_num),.ope
 
 ALU_center  alu_inst (.A_num(Output_A_num1),.B_num(Output_B_num1),.operations_buttons(Output_operations_buttons1),
                .change_mode(Output_change_mode1),.seg1(Oseg1),.seg2(Oseg2),.result_seg(Oresult_seg),
-					.flags_seg(Oflags_seg));
+			   .flags_seg(Oflags_seg));
 
 
 FlipFlop2 ff2_inst (.clk(clk),.reset(reset),.seg1f2(Oseg1),.seg2f2(Oseg2),.result_segf2(Oresult_seg),.flags_segf2(Oflags_seg),
                     .Output_seg1f2(a),.Output_seg2f2(b),.Output_result_segf2(c),.Output_flags_segf2(d));
 
-						  
- always #10 clk = ~clk;
 
-  initial begin
-
-      clk = 0;
-      reset = 0;
-		A_num = 2'b11;
-	   B_num = 2'b01;
-	   operations_buttons = 4'b1110;
-      change_mode = 2'b00; 
-	
-	
-  end
   
- 
 endmodule
