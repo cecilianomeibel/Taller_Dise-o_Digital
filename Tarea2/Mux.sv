61a5c40da99b219de51c2286a9f549858d858e75
Meibel Ceciliano <cecilianomeibel@gmail.com>
2024-03-19 01:20:45 -0600

    Changes in mux

--- Tarea2/Mux.sv
+++ Tarea2/Mux.sv
@@ -1,15 +1,20 @@
 module Mux (
-    input [1:0] data_in, // Entradas de datos
-    input selection, // Señal de selección
-    output reg out // Salida
+    input logic [7:0] total_mant, // recibe como entrada el total de mantenimientos que detecto el cont_mant
+    input sel_state, // Señal de selección
+    output logic [7:0] maintenance // Salida
 );
 
+
+logic [7:0] temp_mant;
+
 always @* begin
-    case(selection)
-        0: out = data_in[0]; // Si selection es 0, la salida es data_in[0]
-        1: out = data_in[1]; // Si selection es 1, la salida es data_in[1]
-        default: out = 1'bX; // Si selection no es ni 0 ni 1, la salida es indefinida
+    case(sel_state)
+        1'b0: temp_mant = 8'b11111111; // Si sel_state es 0, la salida es 0xFF en binario 11111111 (señal de error)
+        1'b1: temp_mant = total_mant; // Si sel_state es 1, la salida es total_mant
     endcase
 end
 
+
+assign maintenance = temp_mant;
 endmodule
+
