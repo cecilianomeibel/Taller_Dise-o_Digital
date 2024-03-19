b32e3748edd50fd7d89db659f7c7dc83a25a4df5
Meibel Ceciliano <cecilianomeibel@gmail.com>
2024-03-19 00:56:47 -0600

    Add logic

--- Tarea2/MantenimientoFSM.sv
+++ Tarea2/MantenimientoFSM.sv
@@ -1,15 +1,21 @@
 module MantenimientoFSM (input clk, rst, m , t0,
-                         output error);
+                         output cont, rst_timer, sel_state );
 								 
 								 
 reg [1:0] state, next_state;
-
+logic temp;
 
 //actual state logic 
 always_ff @ (posedge clk or posedge rst)
    if (rst) state = 2'b00;
 	else 
-	   state = next_state;
+	   state = next_state;	
+	 end
+	    if (state == 2'b01) temp = 1;
+		 else if (state == b'11) temp = 0;
+		 
+	    end 
+		 
 
 		
 //next state logic 
@@ -27,5 +33,8 @@ always_comb
 	 
     endcase
 
+assign cont = (state == 2'b01); // El contador esta en el estado 1	
+assign sel_state = temp;	 
+assign rst_timer = (state == 2'b01 || rst); // rst_timer empieza a funcionar si se esta en el estado 1 o se hace un rst
 	 
 endmodule
