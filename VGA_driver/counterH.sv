b21603bcb0761087fe10f635af960d96e99ce8e9
LudwinR <lujorabri@gmail.com>
2024-04-18 03:07:25 -0600

    It's create a vga driver for reading the state matrix

--- VGA_driver/counterH.sv
+++ VGA_driver/counterH.sv
@@ -1,14 +1,17 @@
-module counterH #(parameter N)(input logic clk, output logic Venable, output logic [N:0] count);
+module counterH #(parameter HACTIVE)(clk, Venable, count);
+	input logic clk;
+	output logic [9:0] count;
+	output logic Venable;
+
 
 	always @(posedge clk) begin
-	
-		if (count < 799) begin
-			count <= count + 1'b1; 
-			Venable <= 1'b0;  // se desabilita el contador vertical cuando se comienza a contar pixeles horizontalmente
-		end 
-		else begin
-			count <= 0;
-			Venable <= 1'b1; // cuando se llega al final de la linea de escaneo H, se habilita el contador vertical
+			if (count < HACTIVE-1) begin
+				count <= count + 1'b1;
+				Venable <= 1'b0;
+			end 
+			else begin
+				count <= 0;
+				Venable <= 1'b1;
+			end
 		end
-	end
-endmodule
+endmodule 
--- VGA_driver/counterV.sv
+++ VGA_driver/counterV.sv
@@ -1,16 +1,14 @@
-module counterV #(parameter N)(input logic clk, input logic Venable, output logic [N:0] count);
+module counterV #(parameter VMAX)(clk, Venable, count);
+	input logic clk, Venable;
+	output logic [9:0] count;	
 
 	always @(posedge clk) begin
-		
-		// se habilita Venable y se cumple la condición cuando se está en el último pixel horizontalmente
 		if (Venable == 1'b1)begin
-		
-			if (count < 524 ) begin
-				count <= count + 1'b1; // se incrementa el contador vertical cuando se está en el úlltimo pixel de una linea horizontal
+			if (count < VMAX-1) begin
+				count <= count + 1'b1;
 			end 
 			else begin
-				count <= 0; // se reinicia el contador cuando se está en el último pixel de una linea horizontal
-							//  y 523 lineas horizontales ya fueron completadas.
+				count <= 0;
 			end
 		end
 	end
