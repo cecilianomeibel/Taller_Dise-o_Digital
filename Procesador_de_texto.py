2b2fde983445cd7e252c98bc7ed3b29d366048eb
Meibel Ceciliano <cecilianomeibel@gmail.com>
2024-06-08 13:11:55 -0600

    Se crea el procesador de texto en python inicialmente, funciona correctamente con una palabra se debe probar con un texto más largo y con espacios intermedios

---
+++ Procesador_de_texto.py
@@ -0,0 +1,49 @@
+# Se ingresa el texto a procesar 
+texto = list("Mariposa.\n")
+fin_texto = len(texto)
+num_vocales = 4
+
+# Función para contar vocales y reemplazar
+def contar_vocales_y_reemplazar(texto):
+    # Inicializar contadores
+    contador_palabras_vocales = 0
+    contador_vocales = 0
+    contador_longitud_palabra = 0
+    
+    i = 0
+    
+    while i < len(texto):
+        # Obtener el carácter actual
+        caracter = texto[i]
+        
+        # Verificar si el carácter es un espacio o un signo de puntuación
+        if caracter in [' ', '.', ',']:
+            if contador_vocales == num_vocales:
+                # Reemplazar vocales en la palabra
+                start = i - contador_longitud_palabra
+                end = i
+                for j in range(start, end):
+                    if texto[j].lower() in ['a', 'e', 'i', 'o', 'u', 'A','E','I','O','U']:
+                        texto[j] = '+'  #se reemplaza por este caracter
+            
+            # Reiniciar contadores
+            contador_vocales = 0
+            contador_longitud_palabra = 0
+        else:
+            # Verificar si el carácter es una vocal
+            if caracter.lower() in ['a', 'e', 'i', 'o', 'u','A','E','I','O','U']:
+                contador_vocales += 1
+            contador_longitud_palabra += 1
+        
+        # Mover al siguiente carácter
+        i += 1
+
+# Llamar a la función para contar vocales y reemplazar
+contar_vocales_y_reemplazar(texto)
+
+# Convertir la lista de nuevo a cadena de texto
+texto_modificado = ''.join(texto)
+
+# Imprimir el texto modificado
+print(texto_modificado)
+
